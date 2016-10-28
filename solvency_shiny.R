b2af3e1ad21f12f12d12e2441cc00dfd50d673a6
awunderground <awunderground@gmail.com>
2016-10-28 12:38:10 -0400

    Add reactive buttons that don't work to shiny application.

--- solvency_shiny.R
+++ solvency_shiny.R
@@ -1,33 +1,50 @@
 library(shiny)
 library(ggplot2)
+library(reshape2)
+
+solvency <- read.csv("solvency.csv", header = TRUE)
+
+
+
+
+
+
 
 ui <- fluidPage(
   
-  selectInput(inputId = "Option",
-              label = "BPC Option Selector", 
-              c("Option 1", "Option 2")),
+  actionButton(inputId = "scheduled", label = "Scheduled"),
+  actionButton(inputId = "mia", label = "Mia"),
+  actionButton(inputId = "tax.ssb", label = "Tax SSB"),
   
   plotOutput("hist")
-  
-  
   )
 
 server <- function(input, output) {
+  
+  data <- eventReactive(input$scheduled, {
+    solvency <-  select(solvency, year, scheduled) 
+    solvency.m <- melt(solvency, id = 1)
+  })
+  
+  data <- eventReactive(input$mia, {
+    solvency <-  select(solvency, year, mini.pia)
+    solvency.m <- melt(solvency, id = 1)
+  })
+  
+  data <- eventReactive(input$mia, {
+    solvency <-  select(solvency, year, tax.ssb)
+    solvency.m <- melt(solvency, id = 1)
+  })
+  
   output$hist <- renderPlot({ 
-    ggplot(cars, aes(x = speed, y = dist)) +
-             geom_point()
+   
+    # Build graphic
+    ggplot(data(), aes(x = year, y = value, colour = variable)) +
+      geom_line(size = 1) +
+      scale_y_continuous(expand = c(0,0)) +
+      ggtitle("Social Security Income/Benefit Ratio for Scheduled Benefits")
+    
     })
 }
 
 shinyApp(ui = ui, server = server)
-
-
-
-
-# Input Function
-
-# checkboxGroupInput()
-# selectInput()
-
-# Ouput Function
-
