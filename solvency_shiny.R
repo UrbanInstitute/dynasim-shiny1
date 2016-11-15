fd12535d53645a3a195667a6516cfdd9ce29eb60
awunderground <awunderground@gmail.com>
2016-11-15 16:33:23 -0500

    Add tooltip to all three charts

--- solvency_shiny.R
+++ solvency_shiny.R
@@ -7,12 +7,12 @@ library(RColorBrewer)
 
 # Source file for Windows
 #Sys.setenv(R_GSCMD = "C:\\Program Files\\gs\\gs9.20\\bin\\gswin64.exe")
-#source('https://raw.githubusercontent.com/UrbanInstitute/urban_R_theme/temp-windows/urban_ggplot_theme.R')
-#source('urban_institute_themes/urban_theme_windows.R')
+#source("https://raw.githubusercontent.com/UrbanInstitute/urban_R_theme/temp-windows/urban_ggplot_theme.R")
+#source("urban_institute_themes/urban_theme_windows.R")
 
 # Source file for Mac
-#source('https://raw.githubusercontent.com/UrbanInstitute/urban_R_theme/master/urban_ggplot_theme.R')
-source('urban_institute_themes/urban_theme_mac.R')
+#source("https://raw.githubusercontent.com/UrbanInstitute/urban_R_theme/master/urban_ggplot_theme.R")
+source("urban_institute_themes/urban_theme_mac.R")
 
 # Load data and gather data into long form for ggplot2
 solvency.m <- read_csv("data/solvency.csv") %>%
@@ -51,15 +51,21 @@ ui <- fluidPage(
     column(4,
            style = "position:relative",
            plotOutput("hist1",
-                      hover = hoverOpts("plot_hover", delay = 100, delayType = "debounce")),
-           uiOutput("hover_info"))),
+                      hover = hoverOpts("plot_hover1", delay = 100, delayType = "debounce")),
+           uiOutput("hover_info1"))),
   
   fluidRow(
     column(4, 
-      plotOutput("hist2")),
+           style = "position:relative",
+           plotOutput("hist2",
+                      hover = hoverOpts("plot_hover2", delay = 100, delayType = "debounce")),
+           uiOutput("hover_info2")),
            
     column(4,
-      plotOutput("hist3")))
+           style = "position:relative",
+           plotOutput("hist3",
+                      hover = hoverOpts("plot_hover3", delay = 100, delayType = "debounce")),
+           uiOutput("hover_info3")))
  
 )
 
@@ -113,11 +119,66 @@ server <- function(input, output) {
     
   })
   
-  output$hover_info <- renderUI({
-    hover <- input$plot_hover
+  # Chart 1
+  output$hover_info1 <- renderUI({
+    hover <- input$plot_hover1
     point <- nearPoints(solvency.m, hover, threshold = 5, maxpoints = 1, addDist = TRUE)
     if (nrow(point) == 0) return(NULL)
         
+    # calculate point position inside the image as percent of total dimensions
+    # from left (horizontal) and from top (vertical)
+    left_pct <- (hover$x - hover$domain$left) / (hover$domain$right - hover$domain$left)
+    top_pct <- (hover$domain$top - hover$y) / (hover$domain$top - hover$domain$bottom)
+    
+    # calculate distance from left and bottom side of the picture in pixels
+    left_px <- hover$range$left + left_pct * (hover$range$right - hover$range$left)
+    top_px <- hover$range$top + top_pct * (hover$range$bottom - hover$range$top)
+    
+    #TODO(awunderground): change CSS colors of pop-up
+    # create style property fot tooltip
+    # background color is set so tooltip is a bit transparent
+    # z-index is set so we are sure are tooltip will be on top
+    style <- paste0("position:absolute; z-index:100; background-color: rgba(245, 245, 245, 0.85); ",
+                    "left:", left_px + 2, "px; top:", top_px + 2, "px;")
+    
+    # actual tooltip created as wellPanel
+    wellPanel(
+      style = style,
+      p(HTML(paste0("<b> Line: </b>", point$variable, "<br/>",
+                    "<b> Ratio: </b>", round(point$value, 2), "<br/>")))
+    )
+  })
+  
+  # Chart 2
+  output$hover_info2 <- renderUI({
+    hover <- input$plot_hover2
+    point <- nearPoints(cost.payroll.m, hover, threshold = 5, maxpoints = 1, addDist = TRUE)
+    if (nrow(point) == 0) return(NULL)
+    
+    print(point)
+    
+    left_pct <- (hover$x - hover$domain$left) / (hover$domain$right - hover$domain$left)
+    top_pct <- (hover$domain$top - hover$y) / (hover$domain$top - hover$domain$bottom)
+    
+    left_px <- hover$range$left + left_pct * (hover$range$right - hover$range$left)
+    top_px <- hover$range$top + top_pct * (hover$range$bottom - hover$range$top)
+    
+    style <- paste0("position:absolute; z-index:100; background-color: rgba(245, 245, 245, 0.85); ",
+                    "left:", left_px + 2, "px; top:", top_px + 2, "px;")
+    
+    wellPanel(
+      style = style,
+      p(HTML(paste0("<b> Line: </b>", point$variable, "<br/>",
+                    "<b> Ratio: </b>", round(point$value, 2), "<br/>")))
+    )
+  })
+  
+  # Chart 3
+  output$hover_info3 <- renderUI({
+    hover <- input$plot_hover3
+    point <- nearPoints(trust.fund.ratio.m, hover, threshold = 5, maxpoints = 1, addDist = TRUE)
+    if (nrow(point) == 0) return(NULL)
+    
     print(point)
     
     left_pct <- (hover$x - hover$domain$left) / (hover$domain$right - hover$domain$left)
